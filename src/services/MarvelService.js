cfbef2c519fad7d761a9d105770929567cc0d99e
NanaLit <nanalit16@gmail.com>
2022-07-20 22:49:44 +0300

    Случайный персонаж добавляется автоматически

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,8 +2,9 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=2251265076cea635643fe65f130ab7a7';
+
     getResourse = async (url) => {
-        let res =await fetch(url);
+        let res = await fetch(url);
 
         if(!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
@@ -11,12 +12,33 @@ class MarvelService {
 
         return await res.json();
     }
-    getAllCharacters = () => {
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+
+    getAllCharacters = async () => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        if (char.description === "") {
+            char.description = "Описание персонажа не найдено";
+        } else if (char.description.length > 200) {
+            char.description = (char.description.slice(0, 199) + '...');
+        } else {
+            char.description = char.description;
+        }
+
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path  + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
