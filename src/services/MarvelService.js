ee715359964cd9b1758794e489b6f6baf4fae1e2
NanaLit <nanalit16@gmail.com>
2022-07-25 21:49:15 +0300

    pagination

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=2251265076cea635643fe65f130ab7a7';
+    _baseOffset = 210;
 
     getResourse = async (url) => {
         let res = await fetch(url);
@@ -12,9 +13,9 @@ class MarvelService {
 
         return await res.json();
     }
-
-    getAllCharacters = async () => {
-        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
